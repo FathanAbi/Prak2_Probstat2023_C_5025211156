b59c7326458a2b2f764c5a7d658b82583ce9afb8
FathanAbi <90834092+FathanAbi@users.noreply.github.com>
2023-06-02 10:33:56 +0700

    Update soal2.R

--- soal2.R
+++ soal2.R
@@ -1,5 +1,11 @@
+library("BSDA")
+# a.
+cat("a.")
 hasil <- zsum.test(mean.x=23500, sigma.x = 3000, n.x = 100,  
           alternative = "less", mu = 25000,
           conf.level = 0.95)
-cat("misal significance level = 1%, p_value =", hasil$p.value, "yang dimana < 0.01 (significance level\n")
-cat("sehingga saya setuju dengan klaim (H0)")
+cat("p_value =", hasil$p.value, "\n")
+
+# c.
+cat("c.")
+cat("P_value", hasil$p.value, " sangat rendah < 0.0001%  (significance value)sehingga saya setuju dengan klaim (H0)")
